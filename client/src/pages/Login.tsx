76cee59fe89f6ce2d1a69ac695a9dcb9fb114bea
Wasim Assad <waseem985asad@gmail.com>
2026-01-19 03:12:12 -0600

    Working in the login page.

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,34 +2,45 @@ import { useState } from "react";
 export default function Login(){
    const[email,setEmail]=useState("");
    const[password,setPassword]=useState("")
+   const[error, setError] = useState("");
+
 
    function handleLogin(){
+      if (email === "" || password === "") {
+         setError("Email and password are required");
+         return;
+      }
+      
+      setError("");
       console.log("Email:",email);
       console.log("password:",password);
    }
+
    return (
-      <div>
-         <h1>Login</h1>
+         <div>
+            <h1>Login</h1>
+            
+            {error && <p style={{ color: "red" }}>{error}</p>}
 
-         <p>Email:</p>
-         <input 
-         type="email" 
-         placeholder="Enter your email"  
-         value={email} 
-         onChange={(e)=> setEmail(e.target.value)}
-         />
+            <p>Email:</p>
+            <input 
+            type="email" 
+            placeholder="Enter your email"  
+            value={email} 
+            onChange={(e)=> setEmail(e.target.value)}
+            />
 
-         <p>Password:</p>
-         <input type="password"
-          placeholder="Enter your Password"
-          value={password}
-          onChange={(e)=> setPassword(e.target.value)} 
-          />
+            <p>Password:</p>
+            <input type="password"
+            placeholder="Enter your Password"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)} 
+            />
 
-          <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogin}>Login</button>
 
-      </div>
-  
-);
+         </div>
+   
+   );
 
 }
