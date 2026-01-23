a21559e17dddb3ecd8fb4ad875ab4d3a1e6b5482
Wasim Assad <waseem985asad@gmail.com>
2026-01-23 01:23:53 -0600

    Impprove the CSS and tsx  for the NoteFound page.

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,3 +1,19 @@
-export default function Notfound(){
-    return <h1>404 - Not found</h1>;
+import { Link } from "react-router-dom";
+import "../styles/NoteFound.css";
+
+export default function NotFound(){
+    return (
+        <div className="notfound-container">
+            <h1 className="notfound-title">404</h1>
+            <h2 className="notfound-subtitle">Page Not Found</h2>
+            
+            <p className="notfound-text">
+                Sorry, the page you’re looking for doesn’t exist or is still under development.
+            </p>
+
+            <Link to="/" className="notfound-button">
+            Go Back Home.
+            </Link>
+        </div>
+    );
 }
